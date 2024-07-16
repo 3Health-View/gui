e6db1fc1a77c72e60effe4cf953e07d517ad6ebb
wue1atwit <wue1@wit.edu>
2024-07-16 15:21:54 -0400

    Added Main component route

--- src/RoutedApp.js
+++ src/RoutedApp.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import App from "./App";
 import AccountForm from "./Components/AccountForm";
+import Main from "./Components/Main";
 
 const RoutedApp = () => {
   return (
     <Routes>
       <Route path="/" element={<App />}>
+        <Route path="" element={<Main />} />
         <Route path="login" element={<AccountForm isLogin={true} />} />
         <Route path="signup" element={<AccountForm />} />
       </Route>
